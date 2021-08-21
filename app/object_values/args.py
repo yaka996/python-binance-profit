88a92b1debe9b1c5451b2d4928e4054f78a9ca01
yaka996 <yaka996@users.noreply.github.com>
2021-08-21 10:15:34 +0100

    Adding limit sell order arg verification

--- app/object_values/args.py
+++ app/object_values/args.py
@@ -6,14 +6,21 @@ from .base import ObjectValue
 class InputArgs(ObjectValue):
     symbol: str
     buy_type: str
-    profit: condecimal(gt=0, le=100)
-    loss: condecimal(gt=0, le=100)
+
 
 
 class LimitInputArgs(InputArgs):
     quantity: condecimal(gt=0)
     price: condecimal(gt=0)
+    profit: condecimal(gt=0, le=100)
+    loss: condecimal(gt=0, le=100)
 
 
 class MarketInputArgs(InputArgs):
     total: condecimal(gt=0)
+    profit: condecimal(gt=0, le=100)
+    loss: condecimal(gt=0, le=100)
+
+class SellLimitInputArgs(InputArgs):
+    quantity: condecimal(gt=0)
+    price: condecimal(gt=0)
